6d715e6e5854ed3e281112002b575c808c04c2ec
Mingchao Zou <zouc@sunyit.edu.com>
2016-03-23 09:50:55 -0400

    Create a client thread that has three type: get, put and delete.

--- kvstore/src/kvstore/SampleClient.java
+++ kvstore/src/kvstore/SampleClient.java
@@ -8,8 +8,47 @@ public class SampleClient {
 	
 	private static String hostname;
 	private static Thread[] clientThread = new Thread[3];
+	private static final int PUT = 0;
+	private static final int GET = 1;
+	private static final int DEL = 2;
 	
-	private static void client(){
+    /**
+     * A client is responsible for retrieving key-value store
+     */
+	private static void clientGet(){
+		try{
+			KVClient client = new KVClient(hostname, 9999);
+
+			Thread curThread = Thread.currentThread();        
+	        System.out.println(curThread.getName() + "get(\"foo\")");
+	        String value = client.get("foo");
+	        System.out.println(curThread.getName() + "Get returned \"" + value + "\"");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(3000));
+	        
+	        System.out.println(curThread.getName() + "get(\"hello\")");
+	        value = client.get("hello");
+	        System.out.println(curThread.getName() + "Get returned \"" + value + "\"");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(3000));
+	        
+	        System.out.println(curThread.getName() + "get(\"how are u\")");
+	        value = client.get("how are u");
+	        System.out.println(curThread.getName() + "Get returned \"" + value + "\"");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(3000));
+
+		}
+        catch (KVException kve) {
+            System.out.println("ERROR: Unexpected KVException raised: ");
+            System.out.println("Message: " + kve.getKVMessage().getMessage());
+            kve.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+    /**
+     * A client is responsible for writing key-value store
+     */
+	private static void clientPut(){
 		try{
 			KVClient client = new KVClient(hostname, 9999);
 
@@ -17,16 +56,17 @@ public class SampleClient {
 	        System.out.println(curThread.getName() + "put(\"foo\", \"bar\")");
 	        client.put("foo", "bar");
 	        System.out.println(curThread.getName() + "put success!");
-	        Thread.sleep(ThreadLocalRandom.current().nextInt(1000));
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
 	        
-	        System.out.println(curThread.getName() + "get(\"foo\")");
-	        String value = client.get("foo");
-	        System.out.println(curThread.getName() + "Get returned \"" + value + "\"");
-	        Thread.sleep(ThreadLocalRandom.current().nextInt(10000));
-
-	        System.out.println(curThread.getName() + "del(\"foo\")");
-	        client.del("foo");
-	        System.out.println(curThread.getName() + "del success!");
+	        System.out.println(curThread.getName() + "put(\"hello\", \"world\")");
+	        client.put("hello", "world");
+	        System.out.println(curThread.getName() + "put success!");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(3000));
+	        
+	        System.out.println(curThread.getName() + "put(\"how are u\", \"fine\")");
+	        client.put("how are u", "fine");
+	        System.out.println(curThread.getName() + "put success!");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(2000));
 
 		}
         catch (KVException kve) {
@@ -38,6 +78,79 @@ public class SampleClient {
         }
 	}
 
+    /**
+     * A client is responsible for deleting key-value store
+     */
+	private static void clientDel(){
+		try{
+			KVClient client = new KVClient(hostname, 9999);
+
+			Thread curThread = Thread.currentThread();
+	        System.out.println(curThread.getName() + "del(\"foo\")");
+	        client.del("foo");
+	        System.out.println(curThread.getName() + "del success!");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(10000));
+	        
+	        System.out.println(curThread.getName() + "del(\"hello\")");
+	        client.del("hello");
+	        System.out.println(curThread.getName() + "del success!");
+	        Thread.sleep(ThreadLocalRandom.current().nextInt(10000));
+		}
+        catch (KVException kve) {
+            System.out.println("ERROR: Unexpected KVException raised: ");
+            System.out.println("Message: " + kve.getKVMessage().getMessage());
+            kve.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+    /**
+     * Create a thread for each client according to its type
+     * @param  Three type of client: put, get and delete
+     */
+	private static Thread createClientFactory(int type){
+		Thread temp = null;
+		switch (type) {
+		case PUT:
+			temp = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while(true){
+						clientPut();
+					}
+				}
+			});
+			return temp;
+		case GET:
+			temp = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					while(true){
+						clientGet();
+					}
+				}
+			});
+			return temp;
+			case DEL:
+				temp = new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						while(true){
+							clientDel();
+						}
+					}
+				});
+				return temp;
+			default:
+			break;
+		}
+		return temp;
+	}
+	
     public static void main(String[] args){
 
         if (args.length != 1) {
@@ -58,15 +171,7 @@ public class SampleClient {
         
         //create three client threads
         for(int i = 0; i < 3; i++){
-        		clientThread[i] = new Thread(new Runnable() {
-					
-					@Override
-					public void run() {
-						while(true){
-							client();
-						}
-					}
-				});
+        		clientThread[i] = createClientFactory(i);
         		clientThread[i].setName("client " + i + ": ");
         		clientThread[i].start();
         }  
